4af23b3527219296f6b9c7ccfd3d6668612aed71
Tien Dat <tiendatbinhan@gmail.com>
2026-04-28 12:22:49 +0700

    feat: install TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { keycloakify } from "keycloakify/vite-plugin";
 
 // https://vitejs.dev/config/
@@ -8,6 +9,7 @@ export default defineConfig({
         react(),
         keycloakify({
             accountThemeImplementation: "none"
-        })
+        }),
+        tailwindcss(),
     ]
 });
